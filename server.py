8c56c5043ef1c5c964b3c6e918bf2cb2a6c18fc6
Wan Liuyang <tsfdye@gmail.com>
2017-11-24 23:32:46 +0800

    Change log level

--- server.py
+++ server.py
@@ -3,20 +3,24 @@ from flask import Flask, request
 from stravalib import Client
 import json
 import os
+import sys
+import logging
 
 app = Flask(__name__)
-
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
 
 def give_kudos(activity_id):
     client = Client(access_token=os.environ.get('STRAVA_ACCESS_TOKEN'))
     athlete = client.get_athlete()
     # activity = client.get_activity(207650614)
-    print('Giving kudos to {}'.format(athlete.username))
-    print('Email: {}'.format(athlete.email))
+    logger.info('Giving kudos to {}'.format(athlete.username))
+    logger.info('Email: {}'.format(athlete.email))
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
-    print('payload = \n{}'.format(request.json))
+    logger.info('payload = \n{}'.format(request.json))
     data = request.json
     activity_id = data['activity_link'].strip().split('/')[-1]
     give_kudos(activity_id)

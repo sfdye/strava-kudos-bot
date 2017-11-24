f80ba1e15152e09b0e158c6b1d73f695d5bcc1f4
Wan Liuyang <tsfdye@gmail.com>
2017-11-25 00:32:30 +0800

    Add mailgun

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
 from __future__ import print_function
-from flask import Flask, request
-from stravalib import Client
+
+import logging
 import json
 import os
 import sys
-import logging
+
+from flask import Flask, request
+import requests
+from stravalib import Client
+
+MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
+MAILGUN_DOMAIN = os.environ.get('MAILGUN_DOMAIN')
+STRAVA_ACCESS_TOKEN = os.environ.get('STRAVA_ACCESS_TOKEN')
+
+
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
+
+
+def send_email(to_addr):
+    return requests.post(
+        "https://api.mailgun.net/v3/{}/messages".format(MAILGUN_DOMAIN),
+        auth=("api", MAILGUN_API_KEY),
+        data={"from": "Excited User <mailgun@YOUR_DOMAIN_NAME>",
+              "to": [to_addr, "YOU@YOUR_DOMAIN_NAME"],
+              "subject": "Hello",
+              "text": "Testing some Mailgun awesomness!"})
 
 def give_kudos(activity_id):
-    client = Client(access_token=os.environ.get('STRAVA_ACCESS_TOKEN'))
+    client = Client(access_token=STRAVA_ACCESS_TOKEN)
     athlete = client.get_athlete()
-    # activity = client.get_activity(207650614)
     logger.info('Giving kudos to {}'.format(athlete.username))
     logger.info('Email: {}'.format(athlete.email))
+    send_email(athlete.email)
 
 @app.route("/webhook", methods=['POST'])
 def webhook():

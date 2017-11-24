cc54cf613e637a99f03a98acc953a7038ae88dd0
Wan Liuyang <tsfdye@gmail.com>
2017-11-25 00:51:55 +0800

    Add sendgrid

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import sys
 
 from flask import Flask, request
 import requests
+import sendgrid
+from sendgrid.helpers.mail import *
 from stravalib import Client
 
 MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
@@ -19,6 +21,19 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.INFO)
+
+
+def sendgrid_email(to_email):
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("test@example.com")
+    subject = "Hello World from the SendGrid Python Library!"
+    to_email = Email(to_email)
+    content = Content("text/plain", "Hello, Email!")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    logger.info(response.status_code)
+    logger.info(response.body)
+    logger.info(response.headers)
 
 
 def send_email(to_addr):
@@ -37,7 +52,7 @@ def give_kudos(activity_id):
     athlete = client.get_athlete()
     logger.info('Giving kudos to {}'.format(athlete.username))
     logger.info('Email: {}'.format(athlete.email))
-    send_email(athlete.email)
+    sendgrid_email(athlete.email)
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
